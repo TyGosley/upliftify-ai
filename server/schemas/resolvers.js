c8f706cc10a101943511001a23122783ae3e14bf
TyGosley <tygosley@gmail.com>
2023-05-18 18:42:57 -0700

    added typeDefs, Resolvers, User/model, and feeling/module

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,66 +1,73 @@
-const { User } = require('../models/User');
+const { User, Feeling } = require('../models/User');
 const { AuthenticationError } = require('apollo-server-express');
 // const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if(context.user) {
-                const userData = await User.findOne({_id: context.user._id})
-                .select('-__v -password')
-                .populate('moods')
-                return userData;
-            }
-            throw new AuthenticationError('Not logged in');
-        }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('feelings').populate;
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
     },
 
-    Mutation: {
-        login: async (parent, {email, password}) => {
-            const user = await User.findOne({email});
-            if(!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const correctPw = await user.isCorrectPassword(password);
-            if(!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const token = signToken(user);
-            return {token, user};
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addFeeling: async (parent, args, context) => {
+      const { emotion, description } = args;
+
+      const newFeeling = new Feeling({ emotion, description });
+
+      await newFeeling.save();
+
+      const updatedUser = await User.findOneAndUpdate(
+        context.user._id,
+        {
+          $push: { feelings: newFeeling._id, emotionHistory: newFeeling._id },
         },
 
-        addUser: async (parent, args) => {  
-            const user = await User.create(args);
-            const token = signToken(user);
-            return {token, user};
-        },
+        { new: true }
+      )
+        .populate('feelings')
+        .populate('emotionHistory');
 
-        // saveMood: async (parent, {input}, context) => {
-        //     if(context.user) {
-        //         const updatedUser = await User.findByIdAndUpdate(
-        //             {_id: context.user._id},
-        //             {$push: {savedMoods: input}},
-        //             {new: true}
-        //         );
-        //         return updatedUser;
-        //     }
-        //     throw new AuthenticationError('You need to be logged in!');
-        // },
+      return newFeeling;
+    },
 
-        // removeMood: async (parent, {moodId}, context) => {
-        //     if(context.user) {
-        //         const updatedUser = await User.findOneAndUpdate(
-        //             {_id: context.user._id},
-        //             {$pull: {savedMood: {moodId: moodId}}},
-        //             {new: true}
-        //         );
-        //         return updatedUser;
-        //     }
-        //     throw new AuthenticationError('You need to be logged in!');
-        // }
-    }
+    deleteEmotionHistory: async (parent, args, context) => {
+      const updatedUser = await User.findByIdAndUpdate(
+        { _id: context.user._id },
+        { $pull: { emotionHistory: [] } },
+        { new: true }
+      )
+        .populate('feelings')
+        .populate('emotionHistory');
+
+      return updatedUser;
+    },
+  },
 };
 
 module.exports = resolvers;
-
-
