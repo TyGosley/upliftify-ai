d59bbcef963f671e0d96087b54d911bb2cde7e1c
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-18 18:40:30 -0700

    initial commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,64 +3,62 @@ const { AuthenticationError } = require('apollo-server-express');
 // const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if(context.user) {
-                const userData = await User.findOne({_id: context.user._id})
-                .select('-__v -password')
-                .populate('moods')
-                return userData;
-            }
-            throw new AuthenticationError('Not logged in');
-        }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('moods');
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
     },
 
-    Mutation: {
-        login: async (parent, {email, password}) => {
-            const user = await User.findOne({email});
-            if(!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const correctPw = await user.isCorrectPassword(password);
-            if(!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const token = signToken(user);
-            return {token, user};
-        },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
 
-        addUser: async (parent, args) => {  
-            const user = await User.create(args);
-            const token = signToken(user);
-            return {token, user};
-        },
+    // saveMood: async (parent, {input}, context) => {
+    //     if(context.user) {
+    //         const updatedUser = await User.findByIdAndUpdate(
+    //             {_id: context.user._id},
+    //             {$push: {savedMoods: input}},
+    //             {new: true}
+    //         );
+    //         return updatedUser;
+    //     }
+    //     throw new AuthenticationError('You need to be logged in!');
+    // },
 
-        // saveMood: async (parent, {input}, context) => {
-        //     if(context.user) {
-        //         const updatedUser = await User.findByIdAndUpdate(
-        //             {_id: context.user._id},
-        //             {$push: {savedMoods: input}},
-        //             {new: true}
-        //         );
-        //         return updatedUser;
-        //     }
-        //     throw new AuthenticationError('You need to be logged in!');
-        // },
-
-        // removeMood: async (parent, {moodId}, context) => {
-        //     if(context.user) {
-        //         const updatedUser = await User.findOneAndUpdate(
-        //             {_id: context.user._id},
-        //             {$pull: {savedMood: {moodId: moodId}}},
-        //             {new: true}
-        //         );
-        //         return updatedUser;
-        //     }
-        //     throw new AuthenticationError('You need to be logged in!');
-        // }
-    }
+    // removeMood: async (parent, {moodId}, context) => {
+    //     if(context.user) {
+    //         const updatedUser = await User.findOneAndUpdate(
+    //             {_id: context.user._id},
+    //             {$pull: {savedMood: {moodId: moodId}}},
+    //             {new: true}
+    //         );
+    //         return updatedUser;
+    //     }
+    //     throw new AuthenticationError('You need to be logged in!');
+    // }
+  },
 };
 
 module.exports = resolvers;
-
-
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,26 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Query {
-        me: User
-    }
+  type Query {
+    me: User
+  }
 
-    type User { 
-        _id: ID
-        username: String
-        email: String
-        password: String
-        
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
 
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    
-
-    type Auth {
-        token: ID!
-        user: User
-    }
-
-    
-
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
